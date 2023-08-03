dcb99df11db407988ae8fec091ed9e87b4906346
DEarniroth <101334675+DEarniroth@users.noreply.github.com>
2023-08-03 19:57:25 +0200

    Update x86_64_arni.c
    ReadMem() felkészítve a nem egymást követő címekre, illetve globális futásleállításra
    WriteMem() felkészítve a nem egymást követő címekre

--- x86_64_arni.c
+++ x86_64_arni.c
@@ -4,27 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void insertAfter(struct MemDataUnit* prev_node, long long int new_data)
-{
-    /*1. check if the given prev_node is NULL */
-    if (prev_node == NULL) {
-        return;
-    }
- 
-    /* 2. allocate new node */
-    struct MemDataUnit* new_node
-        = (struct MemDataUnit*)malloc(sizeof(struct MemDataUnit));
- 
-    /* 3. put in the data */
-    new_node->byte = new_data;
- 
-    /* 4. Make next of new node as next of prev_node */
-    new_node->next = prev_node->next;
- 
-    /* 5. move the next of prev_node as new_node */
-    prev_node->next = new_node;
-}
-
 long long int ReadMem(long long int address, int size)
 {
     //Data memory
@@ -35,8 +14,14 @@ long long int ReadMem(long long int address, int size)
         {
             for (int i = 0; i < size; i++)
             {                
-                toreturn || current->byte << i*8;
+                toreturn |= (long long int)(((current->byte)>>(i*8))&0xFF);
                 current = current->next;
+                if (current->addr != address+i) //Non-following address problem
+                {
+                    Run = 0; //Global error variable; stop running
+                    fprintf(stderr, "Memory read error! Not following address!");
+                    exit(42);
+                }                
             }
             return toreturn;
         }
@@ -44,36 +29,36 @@ long long int ReadMem(long long int address, int size)
         {
             current = current->next;
         }        
-    }    
-    return NULL;
+    }
+    fprintf(stderr, "Memory read error! Data not in memory!");    
+    exit(42);
 }
 
 void WriteMem(long long int address, int size, long long int data)
 {
     //Data memory
+    int i;
     MemDataUnit * current = MemData;
     MemDataUnit * lower = NULL;
-    while (current != NULL) {
-        if (current->addr < address)
-        {
-            lower = current;
-        }        
-        if (current->addr == address)
-        {
-            for (int i = 0; i < size; i++)
-            {                
-                current->byte = data << i*8;
-                current = current->next;
-            }            
-        }
-        else
-        {
-            current = current->next;
-        }
+    MemDataUnit * temp;
+    while (current != NULL && current->addr < address) {
+        lower = current;
+        current = current->next;
     }
-    for (int i = 0; i < size; i++)
-    {
-        insertAfter(lower, data);
+    for (int i = 0; i < size; i++) {
+        if(current != NULL && current->addr == address+i){
+            current->byte = (unsigned char)((data>>(i*8))&0xff);
+        }
+        else{
+            temp = (MemDataUnit*)malloc(sizeof(MemDataUnit));
+            temp->addr = address+i;
+            temp->byte = (unsigned char)((data>>(i*8))&0xff);
+            temp->next = current;
+            if(lower == NULL) MemData = temp;
+            else lower->next = temp;
+            current = temp;
+        }
+        lower = current;
+        current = current->next;
     }
-    return NULL;
 }
