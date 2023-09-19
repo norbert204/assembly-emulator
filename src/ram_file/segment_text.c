947bc825e4c1a7ba55ec65075f54f6c80691ad81
Horváth Norbert <nnorbert204@gmail.com>
2023-09-19 20:07:15 +0200

    Removed main checking from text segment

--- src/ram_file/segment_text.c
+++ src/ram_file/segment_text.c
@@ -96,7 +96,7 @@ void clean_assembly(char *assembly, const size_t size)
     memset(operand2, '\0', 32);
     memset(operand3, '\0', 32);
 
-    sscanf(assembly, "%s%*[ ]%[^,\n]%*[,\n]%[^,\n]%*[,\n]%[^\n]", mnemonic, operand1, operand2, operand3);
+    sscanf(assembly, "%31s%*[ \n]%31[^,\n]%*[,\n]%31[^,\n]%*[,\n]%31[^\n]", mnemonic, operand1, operand2, operand3);
 
     clean_operand(operand1, 32);
     clean_operand(operand2, 32);
@@ -180,7 +180,6 @@ void clean_machine_code(char machine_code[], size_t size)
 
 void handle_text_segment(FILE *input, FILE *output)
 {
-    bool found_main = false;
     bool read_values = false;
 
     char line[512];
@@ -196,7 +195,7 @@ void handle_text_segment(FILE *input, FILE *output)
 
     long last_file_pos = ftell(input);
 
-    while (true)
+    while (!feof(input))
     {
         fgets(line, 512, input);
 
@@ -204,16 +203,6 @@ void handle_text_segment(FILE *input, FILE *output)
         {
             fseek(input, last_file_pos, SEEK_SET);
             break;
-        }
-
-        // We don't start handling the code until we've found the main address
-        if (!found_main)
-        {
-            if (strstr(line, MAIN_FUNCTION_NAME))
-            {
-                found_main = true;
-            }
-            continue;
         }
 
         // We have to append the machine code to the last line
